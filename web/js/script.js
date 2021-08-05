9c9c745d53ff0572b3429415c4c3da736740675d
unknown <danariano83@gmail.com>
2021-08-04 22:15:51 -0300

    Projeto Básico

---
+++ web/js/script.js
@@ -0,0 +1,26 @@
+
+if(window.SimpleSlide) {
+new SimpleSlide({
+  slide: 'quote', // nome do atributo data-slide="principal"
+  time: 5000, // tempo de transição dos slides
+});
+
+new SimpleSlide({
+  slide: 'portfolio',
+  time: 5000,
+  nav: true
+});
+
+}
+
+if(window.SimpleAnime){
+  new SimpleAnime();
+}
+if(window.SimpleForm){
+new SimpleForm({
+  form: ".formphp", //seletor do formulário
+  button: "#enviar",//seletor do botão
+  erro: "<div id='form-erro'><p>Um erro ocorreu, tente para o email contato@bikcraft.com.</p></div>",
+  sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato.</p></div>"
+})
+}
